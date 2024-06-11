305a4ad94381b8c401d7a7e7e1503f8ced51d92b
killpopmusic <tomek.talarczyk@gmail.com>
2024-06-11 12:28:43 +0200

    minor fixes

--- manual_shooting.py
+++ manual_shooting.py
@@ -1,6 +1,3 @@
-'''
-Script for manual shooting command from user, not dependent on the vision 
-'''
 import time
 import threading
 import RPi.GPIO as GPIO
@@ -9,8 +6,6 @@ import RPi.GPIO as GPIO
 servo_pin_yaw = 19  # Pin for yaw servo
 servo_pin_pitch = 18
 shooting_pin = 17  # Pin connected to the shooting gearbox
-
-
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -25,29 +20,25 @@ pwm_yaw.start(0)
 pwm_pitch = GPIO.PWM(servo_pin_pitch, 50)  # 50Hz <-> period = 20ms according to datasheet
 pwm_pitch.start(0)
 
-# Global flag for stopping continuous movement
-stop_continuous_movement = threading.Event()
+# Global flag for controlling shooting
+shooting_event = threading.Event()
 
-#Quick and simple  set angle function 
+# Quick and simple set angle function
 def set_angle(pwm, axis, angle):
-
     duty = (angle / 18) + 2  # Convert angle to duty cycle
-    #GPIO.output(axis, True)
     pwm.ChangeDutyCycle(duty)
     time.sleep(1)
-    #GPIO.output(axis, False)
     pwm.ChangeDutyCycle(0)
 
-# Function to set angle for a yaw  servo slowly
+# Function to set angle for a yaw servo slowly
 def set_angle_smooth(pwm, start_angle, end_angle, increment, delay):
-
     if start_angle < end_angle:
         angle_range = range(start_angle, end_angle + 1, increment)
     else:
         angle_range = range(start_angle, end_angle - 1, -increment)
     
     for angle in angle_range:
-        if stop_continuous_movement.is_set():
+        if shooting_event.is_set():
             break
         duty = (angle / 18) + 2  # Convert angle to duty cycle
         pwm.ChangeDutyCycle(duty)
@@ -61,7 +52,6 @@ def shoot():
     # Set GPIO pin low (0)
     GPIO.output(shooting_pin, GPIO.LOW)
 
-
 def move_yaw():
     # Established parameters for continuous movement
     start_yaw = 10
@@ -70,14 +60,20 @@ def move_yaw():
     delay = 0.03
 
     try:
-        while not stop_continuous_movement.is_set():
+        while True:
+            if shooting_event.is_set():
+                continue
             current_yaw = start_yaw
-            while current_yaw <= end_yaw and not stop_continuous_movement.is_set():
+            while current_yaw <= end_yaw:
+                if shooting_event.is_set():
+                    break
                 set_angle_smooth(pwm_yaw, current_yaw, current_yaw + increment, increment, delay)
                 current_yaw += increment
 
             current_yaw = end_yaw
-            while current_yaw >= start_yaw and not stop_continuous_movement.is_set():
+            while current_yaw >= start_yaw:
+                if shooting_event.is_set():
+                    break
                 set_angle_smooth(pwm_yaw, current_yaw, current_yaw - increment, increment, delay)
                 current_yaw -= increment
 
@@ -85,14 +81,15 @@ def move_yaw():
         pass
 
 def listen_for_stop():
-    while not stop_continuous_movement.is_set():
+    while True:
         user_input = input("Type 's' to shoot ").strip().lower()
         if user_input == "s":
-            stop_continuous_movement.set()
-            set_angle(pwm_pitch,servo_pin_pitch,15)
-            shoot()
+            shooting_event.set()
+            set_angle(pwm_pitch, servo_pin_pitch, 12)
+            #shoot()
             print("Shooting command received")
-
+            set_angle(pwm_pitch, servo_pin_pitch, 25)
+            shooting_event.clear()  # Reset the event to continue yaw movement
 
 # Start the continuous yaw movement in a separate thread
 movement_thread = threading.Thread(target=move_yaw)
@@ -106,4 +103,5 @@ movement_thread.join()
 
 # Cleanup
 pwm_yaw.stop()
+pwm_pitch.stop()
 GPIO.cleanup()
